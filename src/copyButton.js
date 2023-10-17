3a23743d9676b246f4e6f6e0be626bb13c14177d
lilnho <nathanho2048@gmail.com>
2023-10-17 12:45:52 -0700

    Test Commit new line change

--- src/copyButton.js
+++ src/copyButton.js
@@ -2,7 +2,8 @@ import { CopyButton, ActionIcon, Tooltip } from "@mantine/core";
 import { TbCheck, TbCopy } from "react-icons/tb";
 import React from "react";
 function Copy(props) {
-  return ( //returns a copy button that will copy whats passed into props.value to the clipboard
+  return (
+    //returns a copy button that will copy whats passed into props.value to the clipboard
     <CopyButton value={props.value} timeout={2000}>
       {({ copied, copy }) => (
         <Tooltip label={copied ? "Copied" : "Copy"} withArrow position="right">
