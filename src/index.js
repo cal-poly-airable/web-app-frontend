71cf80ea7131b36a7704b2137893e322be8801ce
lilnho <nathanho2048@gmail.com>
2023-11-26 22:13:05 -0800

    Updated index to map to new pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -5,15 +6,36 @@ import Patient from "./Patient";
 import Healthcare from "./Healthcare";
 import Signout from "./Signout";
 import Dev from "./Dev";
-import Home from "./home";
+//import Home from "./home";
+import LandingPage from "./LandingPage";
+import MobileApp from "./MobileApp";
+import HistoryFrame from "./HistoryFrame";
+import Contacts from "./Contacts";
 
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
-const router = createBrowserRouter([ //create/edit routes here
+const router = createBrowserRouter([
+  //create/edit routes here
+  /*{
+    path: "/home",
+    element: <Home />,
+  },*/
   {
     path: "/",
-    element: <Home />,
+    element: <LandingPage />
+  },
+  {
+    path: "/app",
+    element: <MobileApp />
+  },
+  {
+    path: "/history",
+    element: <HistoryFrame />
+  },
+  {
+    path: "/contacts",
+    element: <Contacts />
   },
   {
     path: "/patient",
