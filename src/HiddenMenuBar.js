5553a0cb09e6da3a50f5e257fdf9b55c094a9b49
lilnho <nathanho2048@gmail.com>
2023-12-03 23:33:03 -0800

    Fixed bug with dropdown not appearing

--- src/HiddenMenuBar.js
+++ src/HiddenMenuBar.js
@@ -17,6 +17,22 @@ const HiddenMenuBar = () => {
             const targetElement = entry.target;
             targetElement.classList.add("animate");
             observer.unobserve(targetElement);
+            document.addEventListener("click", (e) => {
+              const isDropdownButton = e.target.matches("[data-dropdown-button]");
+              if (!isDropdownButton && e.target.closest("[data-dropdown]") != null)
+                return;
+          
+              let currentDropdown;
+              if (isDropdownButton) {
+                currentDropdown = e.target.closest("[data-dropdown]");
+                currentDropdown.classList.toggle("active");
+              }
+          
+              document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+                if (dropdown === currentDropdown) return;
+                dropdown.classList.remove("active");
+              });
+            });
           }
         }
       },
