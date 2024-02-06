a5a2acd30ccc0faeed18731c2b4e4c6476b05d99
lilnho <nathanho2048@gmail.com>
2024-02-06 13:02:32 -0800

    Sizing issue

--- src/PatientSide.js
+++ src/PatientSide.js
@@ -360,7 +360,7 @@ function PatientSide(props) {
           {/*setProviderData={setProviderData}*/}
         </div>
         <div>
-          <Row className="graph-buttons">
+          <Row>
             <Col sm={3}>
               <div>
                 <br />
@@ -419,7 +419,7 @@ function PatientSide(props) {
               />
             </Col>
             <Col sm={9}>
-              <CanvasJSChart options={options} style={{padding: 10}} />
+              <CanvasJSChart options={options}  className="graph-buttons" />
             </Col>
           </Row>
         </div>
